92d5180b252aac16311f69878ad796e598bda73c
dev_raphy <63382028+akaRaphael@users.noreply.github.com>
2020-11-19 23:27:49 +0900

    working on art sections

---
+++ modal.js
@@ -0,0 +1,26 @@
+"use strict";
+
+const body = document.querySelector("body");
+const modal_container = document.querySelector(".modal_container");
+const modal_img = document.querySelector(".modal__image");
+const art_page = body.getAttribute("class");
+
+function modalOpen(event) {
+  if (art_page === "art_buddhism") {
+    console.log(art_page);
+    modal_img.style.width = "30vw";
+  }
+
+  const target = event.target.getAttribute("modal");
+  if (target) {
+    modal_img.setAttribute("src", target);
+    modal_container.style.visibility = "visible";
+  }
+}
+
+function modalClose() {
+  modal_container.style.visibility = "hidden";
+}
+
+body.addEventListener("click", modalOpen);
+modal_container.addEventListener("click", modalClose);
